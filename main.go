d0fec3c90205cd0abc3e8dbab166ae0136fed06a
Chris Goffinet <chris@threecomma.io>
2018-08-03 12:01:30 -0700

    Add support for -database and -shards options

--- main.go
+++ main.go
@@ -24,7 +24,10 @@ var (
 	maxLogFileSize int
 	concurrency    int
 	verbose        bool
+	database       string
+	shards         string
 	databases      []*Database
+	shardsOpt      []string
 )
 
 func buildsiRun(wg *sync.WaitGroup, done chan bool, database string, shard int) {
@@ -32,6 +35,7 @@ func buildsiRun(wg *sync.WaitGroup, done chan bool, database string, shard int) 
 
 	log.Printf("Processing (%s) on shard (%d)\n", database, shard)
 
+	// exec influx inspect
 	cmd := exec.Command("influx_inspect", "buildtsi",
 		"-datadir", datadir,
 		"-waldir", waldir,
@@ -39,6 +43,7 @@ func buildsiRun(wg *sync.WaitGroup, done chan bool, database string, shard int) 
 		"-database", database,
 		"-shard", strconv.Itoa(shard))
 
+	// pass 'y' to influx inspect command
 	cmd.Stdin = strings.NewReader("y")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -51,41 +56,77 @@ func buildsiRun(wg *sync.WaitGroup, done chan bool, database string, shard int) 
 	<-done
 }
 
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	flag.StringVar(&datadir, "datadir", "/var/lib/influxdb/data", "datadir location")
 	flag.StringVar(&waldir, "waldir", "/var/lib/influxdb/wal", "waldir location")
 	flag.IntVar(&maxLogFileSize, "max-log-file-size", 131072, "max-log-file-size")
 	flag.IntVar(&concurrency, "concurrency", runtime.GOMAXPROCS(0), "concurrency")
 	flag.BoolVar(&verbose, "verbose", false, "enable verbose mode that prints out stdout from [influx_inspect]")
+	flag.StringVar(&database, "database", "", "run on a specific database (optional)")
+	flag.StringVar(&shards, "shards", "", "run on a specific set of shards (optional)")
 	flag.Parse()
 
+	if shards != "" {
+		shardsOpt = strings.Split(shards, ",")
+	}
+
+	// read datadir for databases
 	path, err := ioutil.ReadDir(datadir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, db := range path {
-		if db.IsDir() && db.Name() != "_internal" {
-			databases = append(databases, &Database{Name: db.Name()})
+	// check if database is optionally passed and use that database if needed
+	if database != "" {
+		databases = append(databases, &Database{Name: database})
+	} else {
+		// iterate over databases on the filesystem and collect those
+		for _, db := range path {
+			if db.IsDir() && db.Name() != "_internal" {
+				databases = append(databases, &Database{Name: db.Name()})
+			}
 		}
 	}
 
+	// iterate over the databases and find shards
 	for _, database := range databases {
-		shards, err := ioutil.ReadDir(filepath.Join(datadir, database.Name, "autogen"))
+		shardsFs, err := ioutil.ReadDir(filepath.Join(datadir, database.Name, "autogen"))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		for _, shard := range shards {
-			if shard.IsDir() {
-				shardNum, _ := strconv.Atoi(shard.Name())
+		// check if shards were optionally passed, if not find them on filesystem
+		if shardsOpt == nil {
+			// find shards on filesystem for the database
+			for _, shard := range shardsFs {
+				if shard.IsDir() {
+					shardNum, _ := strconv.Atoi(shard.Name())
+					database.Shards = append(database.Shards, shardNum)
+				}
+			}
+		} else {
+			// use the defined shards from command-line
+			for _, shard := range shardsOpt {
+				shardNum, _ := strconv.Atoi(shard)
 				database.Shards = append(database.Shards, shardNum)
 			}
 		}
 	}
 
+	// setup a buffered channel with limited concurrency
 	bufCh := make(chan bool, concurrency)
 	wg := new(sync.WaitGroup)
+
+	// iterate over database + shards and launch influx inspect, capping it to concurrency
 	for _, db := range databases {
 		for _, shard := range db.Shards {
 			bufCh <- true
